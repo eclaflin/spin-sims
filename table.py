f79162617315ff8147f982e70c9383315752e22a
eclaflin <eric.claflin@gmail.com>
2024-03-31 17:10:22 -0400

    buildout of child class InsidePocket

--- table.py
+++ table.py
@@ -6,8 +6,10 @@ red = [
 # parent class of inside and outside pockets
 class Pocket:
     # need an attribute corresponding to it's "core" position on the grid
-    def __init__(grid_x, grid_y):
-        self.grid_coordinate = (grid_x, grid_y)
+    def __init__(self, grid_x, grid_y):
+        self.pocket_grid_x = grid_x
+        self.pocket_grid_y = grid_y
+        self.grid_coordinate = (self.pocket_grid_x, self.pocket_grid_y)
 
 class InsidePocket(Pocket):
     def __init__(self, grid_x, grid_y, p_number): 
@@ -15,12 +17,11 @@ class InsidePocket(Pocket):
         
         self.p_number = p_number
 
-    def is_winning(spin_result):
+    def is_winning(self, spin_result):
 
         if spin_result == self.p_number:
             return True
         
         else:
             return False
-
 
